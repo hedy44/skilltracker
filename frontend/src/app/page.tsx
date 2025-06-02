c50ad87b34f25fb1dd9f490c6a448a3948749add
hedy44 <hdiniz4@gmail.com>
2025-06-02 21:03:08 +0100

    feat: add user input with state and dynamic Greeting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+import { useState } from 'react' ;
 import Greeting from '../components/Greeting'
 export default function Home() {
+  const [name, setName] =useState('');
   return (
     <main>
       <h1>Skilltracker MVP</h1>
-       <Greeting name="Hernani" />
+      <input 
+        type="text" 
+        placeholder='Your name' 
+        value={name} 
+        onChange={e => setName(e.target.value)} />
+       <Greeting name={name} 
+       />
       <p>Welcome! This is the beginning of a visionary skill tracking platform.</p>
     </main>
   )
