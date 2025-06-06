cff1709596f347b0b782b3ffe24c04c6acba2726
hedy44 <hdiniz4@gmail.com>
2025-06-06 17:44:27 +0100

    feat: add new skills via input and button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,12 +16,23 @@ export default function Home() {
     {id: 3, name: "Docker"},
   ]);
 
+  const [newSkill, setNewSkill] = useState('');
+
   // 2. Função para remover uma skill (passa o id do skill a remover)
   function handleRemoveSkill(id: number) {
     // Filtra o array para tirar o skill com o id indicado
   setSkills(skills.filter(skill => skill.id !== id));
 }
 
+  function handleAddSkill(){
+    if(!newSkill.trim()) return;
+
+    const novaSkill = {id: Date.now(), name: newSkill};
+
+    setSkills([...skills,novaSkill]);
+
+    setNewSkill('');
+  }
 
   return (
     <main>
@@ -35,7 +46,16 @@ export default function Home() {
        <button onClick={resetName}>Reset Name</button>
        <Greeting name={name}  message="Ready to start your skill journey?"/>
       <p>Welcome! This is the beginning of a visionary skill tracking platform.</p>
-
+      <input
+        type="text"
+        placeholder='Nova skill'
+        value={newSkill}
+        onChange={e => setNewSkill(e.target.value)}
+        />
+        <button onClick={handleAddSkill}
+        >
+          Adicionar Skill 
+        </button>
       <h2>My Skills</h2>
       <div>
         {skills.map(skill => (
