febba45e50dd1955cc34bb17cd41613305eca784
hedy44 <hdiniz4@gmail.com>
2025-07-15 23:45:41 +0100

    Add GET /skills endpoint (list all skills) and ID and Add POST /skills endpoint (create skill)

--- backend/server.ts
+++ backend/server.ts
@@ -6,10 +6,61 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
+// Ver lista de skills (Funcional)
 app.get("/skills", async (req, res) => {
   const skills = await prisma.skill.findMany();
   res.json(skills);
 });
+
+// GET /skills/:id - Buscar uma skill por ID
+app.get("/skills/:id", async (req, res) =>{
+  const {id} = req.params;
+  const skillId = Number(id);
+
+  if(isNaN(skillId)){
+    return res.status(400).json({ error: "Skill ID must be a number"});
+  }
+
+  try {
+    const skill = await prisma.skill.findUnique({
+      where: {id: skillId},
+    });
+
+    if(!skill){
+      return res.status(400).json({ error: "Skill not found"});
+    }
+    res.json(skill);
+    
+  } catch (error) {
+    return res.status(500).json({error: "Internal server error"});
+  }
+})
+
+//Adicionar uma skill (Funcional)
+app.post("/skills", async (req, res)=>{
+  const {name, proficiency} = req.body;
+
+  if(
+    !name || typeof name !== "string" ||
+    !proficiency || typeof proficiency !== "string"
+    ) {
+      return res.status(400).json({ error: "Missing or invalid fields"});
+    }
+
+    try {
+      const newSkill = await prisma.skill.create({
+        data: {
+          name,
+          proficiency,
+        },
+      });
+      return res.status(201).json(newSkill);
+    } catch (error) {
+      return res.status(500).json({error: "Failed to create skill"});
+    }
+    
+  
+})
 
 app.listen(3001, () => {
   console.log("Server running on http://localhost:3001");
